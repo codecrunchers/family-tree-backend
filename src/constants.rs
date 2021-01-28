3d3bdc9a6fe8f36e4be6148c03d89b3851c07cf3
Alan Ryan <alan@codecrunchers.ie>
2021-01-28 22:43:44 +0000

    min server, needs creds & full response as json

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,3 @@
-pub const APPLICATION_JSON: &str = "application/json";
+pub const APPLICATION_JSON: &'static str = "application/json";
+pub const NEO4J_ENDPOINT: &'static str = "http://neo4j:password@localhost:7474/";
+pub const NEO4J_DATABASE: &'static str = "neo4j";
--- src/search.rs
+++ src/search.rs
@@ -3,13 +3,27 @@ use actix_web::HttpResponse;
 //use chrono::{DateTime, Utc};
 //use uuid::Uuid;
 
-use crate::constants::APPLICATION_JSON;
 //use crate::response::Response;
+use rusted_cypher::cypher::result::Row;
+use rusted_cypher::{GraphClient, Statement};
 
 /// search by name /search`
 #[get("/search/{name}")]
 pub async fn search(_name: Path<(String,)>) -> HttpResponse {
+    use crate::constants::{APPLICATION_JSON, NEO4J_DATABASE, NEO4J_ENDPOINT};
+    let graph = GraphClient::connect(NEO4J_ENDPOINT, NEO4J_DATABASE).unwrap();
+
+    let statement = Statement::new("match (p:Person) return p.name, p.id;");
+    let results = graph.exec(statement).unwrap();
+
+    let mut appender: String = String::from("");
+    for row in results.rows() {
+        let name: String = row.get("p.name").unwrap();
+        //println!("name: {}", name);
+        appender = format!("{{'name':'{{{}}}' }}", name.clone());
+    }
+
     HttpResponse::Created()
         .content_type(APPLICATION_JSON)
-        .json("[]".to_string())
+        .json(appender.to_string())
 }
