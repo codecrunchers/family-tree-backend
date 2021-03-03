250b140c13c4b0284f5aec77536e6a5b245ac43c
Alan Ryan <alan@codecrunchers.ie>
2021-03-03 22:13:58 +0000

    add correct cors headers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate actix_web;
+use actix_cors::Cors;
+use actix_web::{http::header, middleware, web, App, HttpServer};
 use std::{env, io};
-
-use actix_web::{middleware, App, HttpServer};
 
 mod constants;
 mod family;
@@ -18,6 +18,21 @@ async fn main() -> io::Result<()> {
         App::new()
             // enable logger - always register actix-web Logger middleware last
             .wrap(middleware::Logger::default())
+            .wrap(
+                Cors::default()
+                    // add specific origin to allowed origin list
+                    .allowed_origin("http://family.snarfel.com:3000")
+                    // set allowed methods list
+                    .allowed_methods(vec!["GET", "POST"])
+                    // set allowed request header list
+                    .allowed_headers(&[header::AUTHORIZATION, header::ACCEPT])
+                    // add header to allowed list
+                    .allowed_header(header::CONTENT_TYPE)
+                    // set list of headers that are safe to expose
+                    .expose_headers(&[header::CONTENT_DISPOSITION])
+                    // set CORS rules ttl
+                    .max_age(3600),
+            )
             // register HTTP requests handlers
             .service(search::search)
             .service(family::family)
